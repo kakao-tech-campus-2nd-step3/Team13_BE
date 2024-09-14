b0771749257d12066e2190dc416e7762ac3261ff
pykido <animoto1@naver.com>
2024-09-15 02:55:23 +0900

    feat : 초기 설정 끝!!

--- src/main/java/dbdr/DbdrApplication.java
+++ src/main/java/dbdr/DbdrApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GyeongdanApplication {
+public class DbdrApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GyeongdanApplication.class, args);
+		SpringApplication.run(DbdrApplication.class, args);
 	}
 
 }
