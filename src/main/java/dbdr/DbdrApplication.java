6e9d479bc7bf9904a69701c49bddd8b8c2f5b0fd
pykido <animoto1@naver.com>
2024-09-15 02:40:38 +0900

    feat : 초기 설정

---
+++ src/main/java/dbdr/DbdrApplication.java
@@ -0,0 +1,13 @@
+package dbdr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GyeongdanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GyeongdanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dbdr/DbdrApplicationTests.java
@@ -0,0 +1,13 @@
+package dbdr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbdrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
