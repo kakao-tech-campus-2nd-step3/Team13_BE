fb4b7c6c92798af1abfca7097cb88e569718c14f
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-09-22 01:04:01 +0900

    feat: 신체 활동 관련 임베디드 타입 생성자 추가

--- src/main/java/dbdr/domain/chart/PhysicalMeal.java
+++ src/main/java/dbdr/domain/chart/PhysicalMeal.java
@@ -1,10 +1,14 @@
 package dbdr.domain.chart;
 
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Embeddable
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class PhysicalMeal {
     // 식사 종류
     private String mealType;
