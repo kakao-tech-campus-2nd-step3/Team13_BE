f8bd2e449d50fd70899974ca4e82a5290ed55813
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-09-22 01:00:43 +0900

    feat: chart-bodyNote 연관관계 설정 - chart와 신체활동(bodyNote)는 일대일 연관관계 설정

--- src/main/java/dbdr/domain/chart/Chart.java
+++ src/main/java/dbdr/domain/chart/Chart.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
@@ -17,6 +18,10 @@ import org.hibernate.annotations.SQLRestriction;
 @SQLRestriction("isDeleted = true")
 public class Chart extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "recipient", nullable = false)
+    @JoinColumn(name = "recipient_id", nullable = false)
     private Recipient recipient;
+
+    @OneToOne
+    @JoinColumn(name = "body_id", nullable = false)
+    private BodyNote bodyNote;
 }
