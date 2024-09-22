a43ecf4d0d5e339deeb951b4b94ba003099ff8c4
mogld <143364802+mogld@users.noreply.github.com>
2024-09-22 18:37:51 +0900

    fix: API 주소 수정

--- src/main/java/dbdr/controller/CareworkerController.java
+++ src/main/java/dbdr/controller/CareworkerController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/careworkers")
+@RequestMapping("/v1/careworkers")
 public class CareworkerController {
 
     private final CareworkerService careworkerService;
