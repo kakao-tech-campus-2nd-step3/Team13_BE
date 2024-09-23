601b38da597f1035a713c1cf6cf1c68a3a804251
mogld <143364802+mogld@users.noreply.github.com>
2024-09-23 14:05:43 +0900

    refactor: chart 도메인 일부 변경

--- src/main/java/dbdr/domain/chart/NursingManagement.java
+++ src/main/java/dbdr/domain/chart/NursingManagement.java
@@ -9,7 +9,7 @@ import org.hibernate.annotations.SQLDelete;
 
 @Entity
 @Table(name = "nursing_management")
-@SQLDelete(sql = "UPDATE nursing_management SET is_deleted = false WHERE id = ?")
+@SQLDelete(sql = "UPDATE nursing_management SET is_active = false WHERE id = ?")
 public class NursingManagement extends BaseEntity {
     @Embedded
     private HealthBloodPressure healthBloodPressure; // 혈압 임베디드 타입
