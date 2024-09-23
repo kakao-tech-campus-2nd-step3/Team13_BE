995c00cd12c269221ba9f07f8c77e988d3eeaf03
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-09-24 00:54:24 +0900

    feat: chartService 작성 - 환자id를 통해 일지 전체 조회 기능 구현

--- src/main/java/dbdr/chart/service/ChartService.java
+++ src/main/java/dbdr/chart/service/ChartService.java
@@ -1,7 +1,22 @@
 package dbdr.chart.service;
 
+import dbdr.chart.domain.Chart;
+import dbdr.chart.dto.response.ChartDetailResponse;
+import dbdr.chart.repository.ChartRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ChartService {
+    private final ChartRepository chartRepository;
+
+    public ChartService(ChartRepository chartRepository) {
+        this.chartRepository = chartRepository;
+    }
+
+    public Page<ChartDetailResponse> getRecipients(Long recipientId, Pageable pageable) {
+        Page<Chart> results = chartRepository.findAllByRecipientId(recipientId, pageable);
+        return results.map(ChartDetailResponse::fromEntity);
+    }
 }
