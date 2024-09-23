81ccc152b64281c1b8f24c2ebde1db4d2565db41
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-09-23 20:53:11 +0900

    feat: chart detail response dto 작성

---
+++ src/main/java/dbdr/dto/response/chart/ChartDetailResponse.java
@@ -0,0 +1,9 @@
+package dbdr.dto.response.chart;
+
+public class ChartDetailResponse {
+    private Long chartId;
+    private BodyManagementResponse bodyManagement;
+    private NursingManagementResponse nursingManagement;
+    private CognitiveManagementResponse cognitiveManagement;
+    private RecoveryTrainingResponse recoveryTraining;
+}
