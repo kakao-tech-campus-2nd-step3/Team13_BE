84d19601618711949f931d674d3085cc01c32141
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-09-23 20:09:56 +0900

    feat: 신체 활동 세부 조회 dto 생성자 추가

--- src/main/java/dbdr/dto/response/BodyManagementResponse.java
+++ src/main/java/dbdr/dto/response/BodyManagementResponse.java
@@ -1,8 +1,10 @@
 package dbdr.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class BodyManagementResponse {
     private Long id;
 
