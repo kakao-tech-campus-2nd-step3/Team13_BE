359bdfc6bd2a4dd9bb1cac7ba55db79a1b5745e7
mac <20j.code@gmail.com>
2024-09-23 16:18:50 +0900

    feat : security config 추가

--- src/main/java/dbdr/config/SecurityConfig.java
+++ src/main/java/dbdr/config/SecurityConfig.java
@@ -1,40 +1,60 @@
 package dbdr.config;
 
-import dbdr.testService.SecurityTestService;
+import dbdr.security.JwtFilter;
+import dbdr.security.JwtProvider;
+import dbdr.security.LoginFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
 @Configuration
-@EnableWebSecurity //이것도 필요?
+@EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
 
-    private final SecurityTestService securityTestService;
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final JwtProvider jwtProvider;
+
 
     enum Role {
         GUARDIAN, CAREWORKER;
     }
 
     @Bean
+    public AuthenticationManager authenticationManager(
+        AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-            .headers((headers) -> headers.frameOptions(FrameOptionsConfig::disable))
-            .authorizeHttpRequests((authorize) -> authorize.anyRequest().hasRole(Role.GUARDIAN.name()))
-            .userDetailsService(securityTestService);
+            .formLogin(AbstractAuthenticationFilterConfigurer::disable)
+            .authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
+
+            .sessionManagement(
+                (session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .addFilterBefore(new JwtFilter(jwtProvider), LoginFilter.class)
+            .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtProvider), UsernamePasswordAuthenticationFilter.class);
         return http.build();
 
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
