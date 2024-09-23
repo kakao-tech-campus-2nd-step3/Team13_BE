e3a7f98e1cab36b5b90aeea95c82efc3431548e1
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-09-23 20:24:20 +0900

    feat: 회복 훈련 response dto 작성

---
+++ src/main/java/dbdr/dto/response/chart/RecoveryTrainingResponse.java
@@ -0,0 +1,13 @@
+package dbdr.dto.response.chart;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class RecoveryTrainingResponse {
+    private Long id;
+    private String recoveryProgram; // 회복 프로그램 이름
+    private boolean recoveryTraining; // 회복훈련 완료 여부
+    private String recoveryNote; // 회복훈련 특이사항
+}
