254fd3ce7067c53f205a4f1c4fd5e13ccc15d0f9
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-09-23 20:10:34 +0900

    feat: 간호 관리 세부 조회 dto 생성자 추가

---
+++ src/main/java/dbdr/dto/response/NursingManagementResponse.java
@@ -0,0 +1,17 @@
+package dbdr.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class NursingManagementResponse {
+    private Long id;
+
+    private int systolic;  // 혈압 최고
+    private int diastolic; // 혈압 최저
+
+    private String healthTemperature; // 체온
+
+    private String healthNote; // 건강 및 간호관리 특이사항
+}
