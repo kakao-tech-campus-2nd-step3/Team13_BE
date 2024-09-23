828ce8157f5b783b584f5436b7129a45663ed549
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-09-23 20:05:40 +0900

    feat: 신체 활동 세부 조회 dto 작성

---
+++ src/main/java/dbdr/dto/response/BodyManagementResponse.java
@@ -0,0 +1,21 @@
+package dbdr.dto.response;
+
+import lombok.Getter;
+
+@Getter
+public class BodyManagementResponse {
+    private Long id;
+
+    private boolean wash; // 세면 유무
+    private boolean bath; // 목욕 유무
+
+    private String mealType; // 식사 종류
+    private String intakeAmount; // 섭취량
+
+    private int physicalRestroom; // 화장실 횟수
+
+    private boolean has_walked; // 산책
+    private boolean has_companion; // 외출 동행
+
+    private String physicalNote; // 특이사항 입력
+}
