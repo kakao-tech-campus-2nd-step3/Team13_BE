40042cc8795969dc0488dc0f3b38c03357916292
pykido <animoto1@naver.com>
2024-09-24 20:53:58 +0900

    refactor : 생성자 오류 해결

--- src/main/java/dbdr/domain/recipient/entity/Recipient.java
+++ src/main/java/dbdr/domain/recipient/entity/Recipient.java
@@ -6,6 +6,7 @@ import dbdr.domain.recipient.dto.request.RecipientRequestDTO;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SQLDelete;
@@ -48,6 +49,28 @@ public class Recipient extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "careworker_id")
     private Careworker careworker;
+
+    @Builder
+    public Recipient(String name,
+        LocalDate birth,
+        String gender,
+        String careLevel,
+        String careNumber,
+        LocalDate startDate,
+        String institution,
+        Long institutionNumber,
+        Careworker careworker) {
+        this.name = name;
+        this.birth = birth;
+        this.gender = gender;
+        this.careLevel = careLevel;
+        this.careNumber = careNumber;
+        this.startDate = startDate;
+        this.institution = institution;
+        this.institutionNumber = institutionNumber;
+        this.careworker = careworker;
+    }
+
 
     public void updateRecipient(RecipientRequestDTO recipientDTO) {
         this.name = recipientDTO.getName();
