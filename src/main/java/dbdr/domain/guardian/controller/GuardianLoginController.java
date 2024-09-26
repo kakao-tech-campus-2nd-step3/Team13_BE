42bc41eeaa1ef47537ce4881989c90b48510b124
mac <20j.code@gmail.com>
2024-09-26 13:47:39 +0900

    feat : 보호자 로그인 기능 추가

--- src/main/java/dbdr/domain/guardian/controller/GuardianLoginController.java
+++ src/main/java/dbdr/domain/guardian/controller/GuardianLoginController.java
@@ -1,8 +1,11 @@
 package dbdr.domain.guardian.controller;
 
+import dbdr.domain.guardian.dto.request.GuardianRequest;
+import dbdr.domain.guardian.service.GuardianLoginService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class GuardianLoginController {
 
+    private final GuardianLoginService GudiandLoginService;
+
     @PostMapping("/login")
-    public ResponseEntity<Void> login() {
-        String token = "token-test";
+    public ResponseEntity<Void> login(@RequestBody GuardianRequest guardianRequest) {
+        String token = GudiandLoginService.login(guardianRequest);
         return ResponseEntity.ok().header("Authorization", token).build();
     }
 }
