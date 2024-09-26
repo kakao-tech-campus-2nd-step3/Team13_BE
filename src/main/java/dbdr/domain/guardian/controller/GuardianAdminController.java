b55affcb348e465c522116c7acf6d1d59efe8a4e
mac <20j.code@gmail.com>
2024-09-26 13:48:40 +0900

    feat : 기존 보호자 도메인 일부 수정 (권한 검사, ID 조회)

--- src/main/java/dbdr/domain/guardian/controller/GuardianAdminController.java
+++ src/main/java/dbdr/domain/guardian/controller/GuardianAdminController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/v1/admin/guardian")
 @RequiredArgsConstructor
+@PreAuthorize("hasRole('GUARDIAN')")
 public class GuardianAdminController {
 
     private final GuardianService guardianService;
--- src/main/java/dbdr/domain/guardian/repository/GuardianRepository.java
+++ src/main/java/dbdr/domain/guardian/repository/GuardianRepository.java
@@ -1,9 +1,12 @@
 package dbdr.domain.guardian.repository;
 
 import dbdr.domain.guardian.entity.Guardian;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GuardianRepository extends JpaRepository<Guardian, Long> {
 
     boolean existsByPhone(String phone);
+
+    Optional<Guardian> findByPhone(String phone);
 }
