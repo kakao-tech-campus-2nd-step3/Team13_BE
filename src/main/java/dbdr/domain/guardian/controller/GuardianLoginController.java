c341be87eda9a7bc59a80786216a8926654d6b95
mac <20j.code@gmail.com>
2024-09-27 18:56:21 +0900

    fix : valid 추가

--- src/main/java/dbdr/domain/guardian/controller/GuardianLoginController.java
+++ src/main/java/dbdr/domain/guardian/controller/GuardianLoginController.java
@@ -2,6 +2,7 @@ package dbdr.domain.guardian.controller;
 
 import dbdr.domain.guardian.dto.request.GuardianRequest;
 import dbdr.domain.guardian.service.GuardianLoginService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +18,7 @@ public class GuardianLoginController {
     private final GuardianLoginService GudiandLoginService;
 
     @PostMapping("/login")
-    public ResponseEntity<Void> login(@RequestBody GuardianRequest guardianRequest) {
+    public ResponseEntity<Void> login(@RequestBody @Valid GuardianRequest guardianRequest) {
         String token = GudiandLoginService.login(guardianRequest);
         return ResponseEntity.ok().header("Authorization", token).build();
     }
