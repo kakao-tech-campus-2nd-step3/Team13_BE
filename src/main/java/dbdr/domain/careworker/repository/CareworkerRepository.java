710d792aaed7b2f4dbf9c69b12a53d10255ba022
mac <20j.code@gmail.com>
2024-09-27 17:10:34 +0900

    feat : 전화번호 조회 명시적 추가

--- src/main/java/dbdr/domain/careworker/repository/CareworkerRepository.java
+++ src/main/java/dbdr/domain/careworker/repository/CareworkerRepository.java
@@ -1,6 +1,7 @@
 package dbdr.domain.careworker.repository;
 
 import dbdr.domain.careworker.entity.Careworker;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,4 +11,6 @@ public interface CareworkerRepository extends JpaRepository<Careworker, Long> {
     List<Careworker> findByInstitutionId(Long institutionId);
 
     boolean existsByEmail(String email);
+
+    Optional<Careworker> findByPhone(String username);
 }
