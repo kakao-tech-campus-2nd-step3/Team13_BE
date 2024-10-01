487d0f23f52b3b6db3fb4ff540f82ce4590888f1
hyyyh0x <141637975+hyyyh0x@users.noreply.github.com>
2024-10-01 15:14:18 +0900

    [관리자 페이지] 요양원 도메인 CRUD (#39)
    * feat: 요양원 도메인
    
    * refactor: admin에서 developer로 변경
    
    * refactor: developer -> admin
    
    * refactor: admin->institution 요양원이 관리하는 부분

---
+++ src/main/java/dbdr/domain/institution/entity/Institution.java
@@ -0,0 +1,42 @@
+package dbdr.domain.institution.entity;
+
+import dbdr.domain.core.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Getter
+@Table(name = "institutions")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE institutions SET is_active = false WHERE id = ?")
+@SQLRestriction("is_active = true")
+public class Institution extends BaseEntity {
+    @Column(unique = true)
+    private String loginId;
+
+    private String loginPassword;
+
+    @Column(nullable = false, unique = true)
+    private Long institutionNumber;
+
+    @Column(nullable = false, length = 100)
+    private String institutionName;
+
+    @Builder
+    public Institution(Long institutionNumber, String institutionName) {
+        this.institutionNumber = institutionNumber;
+        this.institutionName = institutionName;
+    }
+
+    public void updateInstitution(Long institutionNumber, String institutionName) {
+        this.institutionNumber = institutionNumber;
+        this.institutionName = institutionName;
+    }
+}
