28233f33f70d3dc270c93eff22e8a1ff1cc51569
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-10-23 01:01:25 +0900

    feat: jwtFilter에서 인증 오류 시 오류 던지기

--- src/main/java/dbdr/security/config/JwtFilter.java
+++ src/main/java/dbdr/security/config/JwtFilter.java
@@ -1,6 +1,11 @@
 package dbdr.security.config;
 
 import dbdr.security.service.JwtProvider;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.SignatureException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -20,7 +25,7 @@ public class JwtFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
-        FilterChain filterChain) throws ServletException, IOException {
+                                    FilterChain filterChain) throws ServletException, IOException {
 
         String token = request.getHeader("Authorization");
 
@@ -29,8 +34,12 @@ public class JwtFilter extends OncePerRequestFilter {
                 //jwtProvider.validateToken(token);
                 Authentication authentication = jwtProvider.getAuthentication(token);
                 SecurityContextHolder.getContext().setAuthentication(authentication);
+            } catch (ExpiredJwtException | UnsupportedJwtException | MalformedJwtException |
+                     SignatureException | SecurityException | IllegalArgumentException ex) {
+                throw ex;
             } catch (Exception e) {
                 log.debug("토큰 유저 정보 추출 실패 : {}", e.getMessage());
+                throw new JwtException("유효하지 않은 토큰입니다.");
             }
         }
 
