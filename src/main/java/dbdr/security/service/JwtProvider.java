cd63b5e103b68662d82167949aab45669d49a95a
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-10-22 22:06:50 +0900

    feat: refresh 토큰 생성 구현 - 액세스 토큰과 리프레시 토큰을 한 번에 생성

--- src/main/java/dbdr/security/service/JwtProvider.java
+++ src/main/java/dbdr/security/service/JwtProvider.java
@@ -1,10 +1,14 @@
 package dbdr.security.service;
 
+import static dbdr.global.util.api.JwtUtils.ACCESS_TOKEN_EXPIRATION_TIME;
+import static dbdr.global.util.api.JwtUtils.REFRESH_TOKEN_EXPIRATION_TIME;
+
 import dbdr.global.exception.ApplicationError;
 import dbdr.global.exception.ApplicationException;
 import dbdr.global.util.api.JwtUtils;
 import dbdr.security.Role;
 import dbdr.security.dto.BaseUserDetails;
+import dbdr.security.dto.TokenDTO;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import java.nio.charset.StandardCharsets;
@@ -21,11 +25,13 @@ public class JwtProvider {
 
     private final SecretKey secretKey;
     private final BaseUserDetailsService baseUserDetailsService;
+    private final RedisService redisService;
 
     public JwtProvider(@Value("${spring.jwt.secret}") String secret,
-                       BaseUserDetailsService baseUserDetailsService) {
+                       BaseUserDetailsService baseUserDetailsService, RedisService redisService) {
         this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
         this.baseUserDetailsService = baseUserDetailsService;
+        this.redisService = redisService;
     }
 
     public String getUserName(String token) {
@@ -38,6 +44,15 @@ public class JwtProvider {
 
     public boolean isExpired(String token) {
         return getJwtsBody(token).getExpiration().before(new Date());
+    }
+
+    public TokenDTO createAllToken(String username, String role) {
+        TokenDTO token = TokenDTO.builder()
+                .refreshToken(createToken(username, role, REFRESH_TOKEN_EXPIRATION_TIME))
+                .accessToken(createToken(username, role, ACCESS_TOKEN_EXPIRATION_TIME))
+                .build();
+        redisService.saveRefreshToken(username, token.refreshToken());
+        return token;
     }
 
     public String createToken(String username, String role, Long expireTime) {
