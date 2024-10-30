d09048b0c2be68b04863f8ed2dca2a13be58edbb
hyyyh0x <hyyyh0x@gmail.com>
2024-10-30 15:35:29 +0900

    feat: swagger에 security 추가

--- src/main/java/dbdr/global/configuration/OpenApiConfiguration.java
+++ src/main/java/dbdr/global/configuration/OpenApiConfiguration.java
@@ -3,6 +3,8 @@ package dbdr.global.configuration;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,13 +14,25 @@ public class OpenApiConfiguration {
 
     @Bean
     public OpenAPI openAPI() {
+        String jwt = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
+        SecurityScheme securityScheme = new SecurityScheme()
+            .name(jwt)
+            .type(SecurityScheme.Type.HTTP)
+            .scheme("Bearer")
+            .bearerFormat("JWT");
+
+        Components components = new Components().addSecuritySchemes(jwt, securityScheme);
+
         Info info = new Info()
             .title("돌봄다리 API Document")
             .version("v1.0.0")
             .description("돌봄다리 서버 API 명세서입니다.");
+
         return new OpenAPI()
-            .components(new Components())
-            .info(info);
+            .components(components)
+            .info(info)
+            .addSecurityItem(securityRequirement);
     }
 
     @Bean
