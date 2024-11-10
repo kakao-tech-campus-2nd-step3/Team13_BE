d9f4a1994d585d0ba85f358896f8ffd25a1e2825
pykido <animoto1@naver.com>
2024-11-11 02:36:52 +0900

    feat : health-check 추가 및 CORS 해결

--- src/main/java/dbdr/global/configuration/WebConfig.java
+++ src/main/java/dbdr/global/configuration/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
-			.allowedOrigins("*") // 모든 도메인 허용
+			.allowedOriginPatterns("*") // allowCredentials 사용 시 '*' 대신 allowedOriginPatterns 사용
 			.allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드 지정
 			.allowedHeaders("*") // 모든 헤더 허용
 			.allowCredentials(true); // 인증 정보 허용
--- src/main/java/dbdr/security/config/SecurityConfig.java
+++ src/main/java/dbdr/security/config/SecurityConfig.java
@@ -57,6 +57,8 @@ public class SecurityConfig {
                 .authenticationProvider(baseAuthenticationProvider())
                 .authorizeHttpRequests((authorize) -> {
                     authorize
+                        .requestMatchers("/actuator/health")
+                        .permitAll()
                         .requestMatchers("/v1/ocr/**")
                         .permitAll()
                         .requestMatchers("/v1/admin/**")
