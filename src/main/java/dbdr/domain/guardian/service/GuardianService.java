95e185f1e833e6183203d76397f9980697c2d6ca
pykido <animoto1@naver.com>
2024-11-14 19:18:44 +0900

    refactor : 간단한 수정

--- src/main/java/dbdr/domain/guardian/service/GuardianService.java
+++ src/main/java/dbdr/domain/guardian/service/GuardianService.java
@@ -1,6 +1,5 @@
 package dbdr.domain.guardian.service;
 
-import dbdr.domain.core.alarm.service.AlarmService;
 import dbdr.domain.guardian.dto.request.GuardianMyPageRequest;
 import dbdr.domain.guardian.dto.request.GuardianUpdateRequest;
 import dbdr.domain.guardian.dto.response.GuardianMyPageResponse;
@@ -127,25 +126,6 @@ public class GuardianService {
     }
 
     @Transactional
-    public GuardianResponse addGuardianByInstitution(GuardianRequest guardianRequest, Long institutionId) {
-        ensureUniquePhone(guardianRequest.phone());
-        Institution institution = institutionRepository.findById(institutionId)
-            .orElseThrow(() -> new ApplicationException(
-                ApplicationError.INSTITUTION_NOT_FOUND));
-        String password = passwordEncoder.encode(guardianRequest.loginPassword());
-        Guardian guardian = Guardian.builder().phone(guardianRequest.phone())
-            .name(guardianRequest.name())
-            .phone(guardianRequest.phone())
-            .loginPassword(password)
-            .institution(institution)
-            .build();
-        guardian = guardianRepository.save(guardian);
-        alarmService.createGuardianAlarm(guardian);
-        return new GuardianResponse(guardian.getId(), guardian.getPhone(), guardian.getName(),
-            guardian.getInstitution().getId(), guardian.isActive());
-    }
-
-    @Transactional
     public void deleteGuardianById(Long guardianId) {
         Guardian guardian = findGuardianById(guardianId);
         guardian.deactivate();
@@ -186,7 +166,7 @@ public class GuardianService {
         guardianRepository.save(guardian);
     }
 
-	public List<Guardian> findByAlertTime(LocalTime currentTime) {
+    public List<Guardian> findByAlertTime(LocalTime currentTime) {
         return guardianRepository.findByAlertTime(currentTime);
-	}
+    }
 }
